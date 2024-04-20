f08c189a8621946bc575ac8d5e157fb5d522fed8
Joost van der Laan <j.vanderlaan1@students.uu.nl>
2024-04-20 22:24:33 +0200

    Back-end tework and added Gift Ranks

--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,19 @@ struct DomainSettings {
 
 #[derive(Default,Clone)]
 struct GiftChance {
-    power:Vec<f64>,
-    bonus:Vec<f64>,
-    quick:Vec<f64>,
-    bounty:Vec<f64>
+    power:Vec<AverageRank>,
+    bonus:Vec<AverageRank>,
+    quick:Vec<AverageRank>,
+    bounty:Vec<AverageRank>
 }
+
+type AverageRank = [f64;3];
 
 struct Probabilities {
     chosen:[f64;3],
 }
 
+#[derive(Copy,Clone,Debug)]
 enum GType {
     Power,
     Bonus,
@@ -49,7 +52,7 @@ enum GRank {
 }
 
 const POWER_COLORS:[&str;3] = ["#2a2a59","#44338e","#9b73d6"];
-const BONUS_COLORS:[&str;3] = ["#f0892b","#eabd57","#f0892b"];
+const BONUS_COLORS:[&str;3] = ["#f0892b","#eabd57","#e3dc66"];
 const QUICK_COLORS:[&str;3] = ["#069c80","#0ebc80","#71e380"];
 
 fn gift_color(gift_type:GType, rank:usize)-> Color32 {
@@ -61,21 +64,16 @@ fn gift_color(gift_type:GType, rank:usize)-> Color32 {
     egui::ecolor::Color32::from_hex(hex_color).unwrap()
 }
 
-fn apply_probability(remaining:&mut [f64;3], chance:f64) -> f64 {
-    let mut total_added = 0.0;
-    let mut accumulated_prob = 1.0;
-    for i in 0..=2 {
-        let current = remaining[i] * chance * accumulated_prob;
-        
-        //added to later slots if current slot was full
-        accumulated_prob *= 1.0 - remaining[i];
+// ------------------- GIFT SPECIFIC FUNCTIONS -----------------
 
-        //fill up current slot
-        total_added += current;
-        remaining[i] -= current;
+fn gift_probabilities(karma:f64, gift_type:GType) -> Probabilities {
+    match gift_type {
+        GType::Power => power_probabilities(karma),
+        GType::Bonus => bonus_probabilities(karma),
+        GType::Quick => quick_probabilities(karma)
     }
-    total_added
 }
+
 
 fn power_probabilities(karma: f64) -> Probabilities {
     Probabilities {
@@ -107,6 +105,8 @@ fn quick_probabilities(karma: f64) -> Probabilities {
     }
 }
 
+// -------------- ARITHMATIC FUNCTIONS ---------------
+
 fn clamp(i:f64, min:f64, max:f64) -> f64 {
     if i < min {
         min
@@ -117,6 +117,52 @@ fn clamp(i:f64, min:f64, max:f64) -> f64 {
     }
 }
 
+fn apply_probability(remaining:&mut [f64;3], chance:f64) -> f64 {
+    let mut total_added = 0.0;
+    let mut accumulated_prob = 1.0;
+    for i in 0..=2 {
+        let current = remaining[i] * chance * accumulated_prob;
+        
+        //added to later slots if current slot was full
+        accumulated_prob *= 1.0 - remaining[i];
+
+        //fill up current slot
+        total_added += current;
+        remaining[i] -= current;
+    }
+    total_added
+}
+
+/// Calculates the average number of gifts of each type after several "Try add gift" actions in a specific order
+/// 
+fn try_gift_sequence(karma:f64, order:&[GType]) -> [AverageRank;3] {
+    //data per gift
+    let mut frequency = [0,0,0];
+    let mut result = [[0.0;3];3];
+
+    //open gift slots
+    let mut remaining = [1.0, 1.0, 1.0];
+
+    for gift_elem in order {
+        let gift_index = *gift_elem as usize;
+        let gift_freq = frequency[gift_index];
+        if gift_freq >= 3 {
+            panic!("Gift {gift_elem:?} occurred in the order list more than 3 times!\nlist:{order:?}")
+        }
+        let gift_chance = gift_probabilities(karma, *gift_elem).chosen[gift_freq];
+        let gifts_added = apply_probability(&mut remaining, gift_chance);
+
+        result[gift_index][0] += gifts_added;
+
+        //different rank calculations using gifts_added
+
+        frequency[gift_index] += 1;
+    }
+    return result;
+}
+
+
+// ------------ PROGRAM ----------------
 impl PlotProgram {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // egui_extras::install_image_loaders(&cc.egui_ctx);
@@ -126,7 +172,7 @@ impl PlotProgram {
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
         let mut program = PlotProgram {
-            domain_settings:DomainSettings { min: 1, max: 200, step: 1 },
+            domain_settings:DomainSettings { min: 0, max: 200, step: 1 },
             ..Self::default()};
         program.recalc();
         program
@@ -137,6 +183,7 @@ impl PlotProgram {
         self.karma_range = (settings.min..=settings.max).step_by(settings.step as usize).collect();
         self.recalc_giftchance();
     }
+
 
     fn recalc_giftchance(&mut self) {
         let karma_range = &self.karma_range;
@@ -182,10 +229,10 @@ impl PlotProgram {
 
             let mut bounty_elem = 1.0;
 
-            power.push(power_elem);
-            bonus.push(bonus_elem);
-            quick.push(quick_elem);
-            bounty.push(bounty_elem);
+            power.push([power_elem, power_elem/2.0, power_elem/4.0]);
+            bonus.push([bonus_elem, bonus_elem/2.0, bonus_elem/4.0]);
+            quick.push([quick_elem, quick_elem/2.0, quick_elem/4.0]);
+            bounty.push([bounty_elem, bounty_elem/2.0, bounty_elem/4.0]);
             
 
 
@@ -193,9 +240,30 @@ impl PlotProgram {
         self.gift_chance = GiftChance {power,bonus, quick,bounty};
     }
 
-    
+    fn gift_chart(&self, gift_type:GType, average_ranks:&Vec<AverageRank>, name:&str) -> [plt::BarChart;3] {
+        let simple_gift =       self.single_rank_chart(gift_color(gift_type,0), 0, &average_ranks, &format!("{name} 1 star"));
+        let lovely_gift =       self.single_rank_chart(gift_color(gift_type,1), 1, &average_ranks, &format!("{name} 2 star"));
+        let wonderful_gift =    self.single_rank_chart(gift_color(gift_type,2), 2, &average_ranks, &format!("{name} 3 star"));
+        [simple_gift, lovely_gift, wonderful_gift]
+    }
+
+    fn single_rank_chart(&self, color:Color32,rank:usize, average_ranks:&Vec<AverageRank>, name:&str) -> plt::BarChart {
+        plt::BarChart::new(
+            self.karma_range.iter().enumerate().map(|(i,karma)| {
+                let width = self.domain_settings.step as f64;
+                plt::Bar::new(*karma as f64, average_ranks[i][rank])
+                    .width(width + 0.2)
+                    .fill(color)
+        }).collect()).name(name).color(color)
+    }
 
 }
+
+
+// ----------------------- USER INTERACTION -------------------------
+
+
+
 impl eframe::App for PlotProgram {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -227,31 +295,12 @@ impl eframe::App for PlotProgram {
             let bonus_color = egui::ecolor::Color32::from_hex("#e3dc66").unwrap();
             let quick_color = egui::ecolor::Color32::from_hex("#51da6d").unwrap();
 
-            let power_gifts : egui_plot::BarChart = plt::BarChart::new(
-                self.karma_range.iter().enumerate().map(|(i,karma)| {
-                    let width = self.domain_settings.step as f64;
-                    plt::Bar::new(*karma as f64, self.gift_chance.power[i])
-                        .width(width + 0.2)
-                        .fill(power_color)
-            }).collect()).name("power_gift").color(power_color);
+            let power_gifts = self.gift_chart(GType::Power,&self.gift_chance.power,"power");
+            let bonus_gifts = self.gift_chart(GType::Bonus,&self.gift_chance.bonus,"bonus").map(|c| c.stack_on(&[&power_gifts[0]]));
+            let quick_gifts = self.gift_chart(GType::Quick,&self.gift_chance.quick,"quick").map(|c| c.stack_on(&[&bonus_gifts[0]]));
 
-            let bonus_gifts : egui_plot::BarChart = plt::BarChart::new(
-                self.karma_range.iter().enumerate().map(|(i,karma)| {
-                    let width = self.domain_settings.step as f64;
-                    plt::Bar::new(*karma as f64, self.gift_chance.bonus[i])
-                        .width(width + 0.2)
-                        .fill(bonus_color)
-                        .stroke(eframe::epaint::Stroke {width:1.0, color: bonus_color})
-            }).collect()).name("bonus_gift").color(bonus_color).stack_on(&[&power_gifts]);
 
-            let quick_gifts : egui_plot::BarChart = plt::BarChart::new(
-                self.karma_range.iter().enumerate().map(|(i,karma)| {
-                    let width = self.domain_settings.step as f64;
-                    plt::Bar::new(*karma as f64, self.gift_chance.quick[i])
-                        .width(width + 0.2)
-                        .fill(quick_color)
-            }).collect()).name("quick_gift").color(quick_color).stack_on(&[&bonus_gifts]);
-
+            let chart_list = [power_gifts, bonus_gifts, quick_gifts];
             egui_plot::Plot::new("my_plot")
                 .view_aspect(2.0)
                 .allow_drag(false)
@@ -261,9 +310,11 @@ impl eframe::App for PlotProgram {
                 .y_axis_label("average amount")
                 .legend(plt::Legend::default().position(plt::Corner::LeftTop))
                 .show(ui, |plot_ui| {
-                    plot_ui.bar_chart(power_gifts);
-                    plot_ui.bar_chart(bonus_gifts);
-                    plot_ui.bar_chart(quick_gifts)
+                    for list in chart_list {
+                        for chart in list {
+                            plot_ui.bar_chart(chart);
+                        }
+                    }
                 });
         });
     }
